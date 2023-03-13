adad5a0c0d5ab0b367515186922a4e9c1f51bd74
Seleroad <118297089+Solblnc@users.noreply.github.com>
2023-03-13 23:46:59 +0300

    structure of app

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import "fmt"
 
+// struct for the pointers
+
+type App struct {
+}
+
+func (app *App) Run() error {
+	return nil
+}
+
+func main() {
+	app := App{}
+	if err := app.Run(); err != nil {
+		fmt.Println(err)
+	}
 }
